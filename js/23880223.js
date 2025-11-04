91e23363fcd887dd93c3916231898404ca8fa8de
giao <maiminhgiao@gmail.com>
2025-11-04 23:09:05 +0700

    251104 send email

--- js/23880223.js
+++ js/23880223.js
@@ -1,4 +1,5 @@
 const API = "https://web1-api.vercel.app/api";
+const AUTHENTICATE_API = "https://web1-api.vercel.app/users";
 
 async function loadData(request, templateId, viewId) {
   const response = await fetch(`${API}/${request}`);
@@ -10,4 +11,24 @@ async function loadData(request, templateId, viewId) {
 
   var view = document.getElementById(viewId);
   view.innerHTML = template(context);
+}
+
+async function getAuthenticateToken(username, password) {
+  const response = await fetch(`${AUTHENTICATE_API}/authenticate`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify({
+      username: username,
+      password: password,
+    }),
+  });
+  let result = response.json();
+  if (response.status === 200) {
+    return result;
+  } else {
+    throw new Error((await result).message);
+  }
 }
